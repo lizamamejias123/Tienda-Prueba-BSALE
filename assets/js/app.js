abbd6a912fe065fadf6403c74435d6e491eb7650
LuisLizamaMejias <60248293+lizamamejias123@users.noreply.github.com>
2021-10-13 13:29:59 -0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+// sumar solamente numero en carrito de compras
+var a = 0;
+function sumar() {
+    a = a + 1;
+    document.getElementById("numero").textContent = a;
+    alert("Añadido al carrito de compras")
+}
+// alerta de usuario
+function login(){
+    alert("Usuario no existe")
+}
+// alerta de servicios no disponible por el momento
+function nuevoUsuario(){
+    alert("Por el momento, no podemos registrar nuevo usuario")
+}
+function recordar(){
+    alert("Servicio No Disponible")
+}
